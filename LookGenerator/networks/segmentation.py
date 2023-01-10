1b8470ecfd003204514ed679e263cdb8239ca505
EgorProkopov <egorprokopov216@gmail.com>
2023-01-10 19:05:58 +0300

    UNet с изменяемым числом слоев написана
    Нужно написать тест для ее обучения

--- LookGenerator/networks/segmentation.py
+++ LookGenerator/networks/segmentation.py
@@ -1,24 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 
-import numpy as np
-import matplotlib.pyplot as plt
+from modules import ConvModule5x5BN
 
 
-#TODO: переписать
-class UNET4(nn.Module):
+class UNet(nn.Module):
     def __init__(
-            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
+            self, in_channels=3, out_channels=3, features=[64, 128, 256, 512],
     ):
-        super(UNET, self).__init__()
+        super(UNet, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Encoder
         for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature))
+            self.downs.append(ConvModule5x5BN(in_channels, feature))
             in_channels = feature
 
         # Decoder
@@ -28,9 +29,9 @@ class UNET4(nn.Module):
                     feature*2, feature, kernel_size=2, stride=2,
                 )
             )
-            self.ups.append(DoubleConv(feature*2, feature))
+            self.ups.append(ConvModule5x5BN(feature*2, feature))
 
-        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.bottleneck = ConvModule5x5BN(features[-1], features[-1]*2)
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def forward(self, x):
@@ -55,3 +56,54 @@ class UNET4(nn.Module):
             x = self.ups[idx+1](concat_skip)
 
         return self.final_conv(x)
+
+
+def train_UNet(model, train_dataloader, val_dataloader, device='cpu', epoch_num=5):
+    model = model.to(device)
+
+    train_history = []
+    val_history = []
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(epoch_num):
+        train_running_loss = 0.0
+        for data, targets in train_dataloader:
+            data = data.to(device)
+            targets = targets.to(device)
+
+            outputs = model(data)
+    
+            optimizer.zero_grad()
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+            train_running_loss += loss.item()
+
+        train_loss = train_running_loss/len(train_dataloader)
+        train_history.append(train_history)
+        print(f'Epoch {epoch} of {epoch_num}, train loss: {train_loss:.3f}')
+
+        val_running_loss = 0.0
+        for data in val_dataloader:
+            data = data.to(device)
+            targets = targets.to(device)
+
+            outputs = model(data)
+            loss = criterion(outputs, targets)
+            val_running_loss = loss.item()
+
+        val_loss = val_running_loss/len(val_dataloader)
+        val_history.append(val_loss)
+        print(f'Epoch {epoch} of {epoch_num}, val loss: {val_loss:.3f}')
+
+
+# TODO: написать тест
+def test_UNet():
+    pass
+
+
+if __name__ == "__main__":
+    test_UNet()
+
