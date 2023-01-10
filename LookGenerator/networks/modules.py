ce7b512f8384281087c0e760b8d8c6ba431da0c1
EgorProkopov <egorprokopov216@gmail.com>
2023-01-10 19:04:54 +0300

    Снова немного подправил модули

--- LookGenerator/networks/modules.py
+++ LookGenerator/networks/modules.py
@@ -55,19 +55,21 @@ class ConvModule5x5(ConvModule3x3):
 
 
 class ConvModule5x5BN(ConvModule3x3):
-    def __init__(self, in_channels, have_bias=True):
+    def __init__(self, in_channels, out_channels=None, have_bias=True):
         super(ConvModule5x5BN, self).__init__(in_channels, have_bias)
-        self.init_net(in_channels, have_bias)
-        #self.init_weights(have_bias)
+        if out_channels == None:
+            out_channels = 2*in_channels
 
-    def init_net(self, in_channels, have_bias):
+        self.init_net(in_channels, out_channels, have_bias)
+
+    def init_net(self, in_channels, out_channels, have_bias):
         self.net = nn.Sequential(
-            nn.Conv2d(in_channels, 2*in_channels, kernel_size=3, padding=1, bias=have_bias),
-            nn.BatchNorm2d(2*in_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=have_bias),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
 
-            nn.Conv2d(2*in_channels, 2*in_channels, kernel_size=3, padding=1, bias=have_bias),
-            nn.BatchNorm2d(2*in_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=have_bias),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
 
