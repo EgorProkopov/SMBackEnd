448f83bc93f06d1b2c54e99370757c034156d875
KlarkGames <laletin.alesha@mail.ru>
2023-01-31 17:25:46 +0300

    Тестирование UNet.shape

---
+++ LookGenerator/tests/UNet_tests.py
@@ -0,0 +1,14 @@
+from LookGenerator.networks.segmentation import UNet
+from PIL import Image
+from pathlib import Path
+from torchvision.transforms import ToTensor
+
+
+def test_output_shape():
+    input_value = Image.open(Path("test_data_samples/cloth.jpg"))
+    transforms = ToTensor()
+    input_value = transforms(input_value)[None, :]
+
+    model = UNet()
+    output_value = model(input_value)
+    assert output_value.shape == (1, 1, 1024, 768)
