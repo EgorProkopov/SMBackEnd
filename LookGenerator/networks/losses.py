f74c79e8ee6047c58e2b7c3a5ba56c47a6ed7f65
EgorProkopov <egorprokopov216@gmail.com>
2023-02-18 21:03:02 +0300

    Написал документацию для нового класса лосса

--- LookGenerator/networks/losses.py
+++ LookGenerator/networks/losses.py
@@ -5,20 +5,28 @@ import torch.nn.functional as functional
 
 
 class IoULoss(nn.Module):
+    """
+    Loss for binary segmentation
+
+    Class of Intersection over Union, or Jaccard Loss
+
+    It is calculated as the ratio between the overlap of
+    the positive instances between two sets, and their mutual combined values
+
+    J(A, B) = |A and B| / |A or B| = |A and B| / (|A| + |B| - |A and B|)
+
+    """
     def __init__(self, weight=None, size_average=True):
         super(IoULoss, self).__init__()
 
     def forward(self, inputs, targets, smooth=1):
 
-        # comment out if your model contains a sigmoid or equivalent activation layer
+        # comment out if model contains a sigmoid or equivalent activation layer
         # inputs = functional.sigmoid(inputs)
 
-        # flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
 
-        # intersection is equivalent to True Positive count
-        # union is the mutually inclusive area of all labels & predictions
         intersection = (inputs * targets).sum()
         total = (inputs + targets).sum()
         union = total - intersection
