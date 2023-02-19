bfc8c29f2e3a1f728d8cc23c5d45c1dabc055346
sidsnow <obobojk@gmail.com>
2023-02-19 15:05:10 +0300

    добавлены вспомогательные методы для подготовки изображений для модели и для отображения результата

--- LookGenerator/datasets/utils.py
+++ LookGenerator/datasets/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot as plt
+import torch
 import numpy as np
 from PIL import Image
 from dataclasses import dataclass
@@ -26,10 +27,55 @@ def convert_channel(image: Image):
     return np.asarray(image.convert('L')) / 255
 
 
+def prepare_image_for_model_transpose(image: Image):
+    """
+    На вход подается трехканальная картинка (высота, ширина, количество каналов)
+    Выдает тензор [новое измерение, количество каналов, ширина, высота]
+    """
+    return prepare_image_for_model(image).T
+
+
 def prepare_image_for_model(image: Image):
-    return np.asarray(image, dtype=np.float32)
+    """
+    На вход подается трехканальная картинка (количество каналов, ширина, высота)
+    На выход подается трехканальная картинка (новое измерение, количество каналов, ширина, высота)
+    """
+    return torch.tensor(np.asarray(image, dtype=np.float32)[np.newaxis, ...])
+
+
+def to_array_from_model_transpose(tensor):
+    """
+    На вход подается тензор из модели [измерение, количество каналов, ширина, высота]
+    На выход получается массив numpy [высота, ширина, количество каналов]
+    """
+    return tensor.detach().numpy()[0, :, :, :].T
 
 
 def show_array_as_image(array: np.array):
     return plt.imshow(array)
 
+
+def show_array_multichannel(array):
+
+    """
+    На вход подается массив numpy [ВЫСОТА, ШИРИНА, КАНАЛЫ]
+    каналов 16!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!, иначе пользоваться предыдущим методов для каждого канала отдельно
+
+    Вспомогательная функция, которая была использована для показа 16 канальных изображений
+    """
+
+    plt.figure(figsize=(18, 6))
+    for i in range(1, 16 // 2 + 1):
+        plt.subplot(1, 8, i)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(array[:, :, i - 1], cmap='gray')
+
+    plt.figure(figsize=(18, 6))
+    ctr = 0
+    for i in range(8, 16):
+        ctr += 1
+        plt.subplot(1, 8, ctr)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(array[:, :, i], cmap='gray')
