9a44ca12603bfef7f1d4696234bac120c9bf6d69
EgorProkopov <egorprokopov216@gmail.com>
2023-02-21 00:20:26 +0300

    Написал threshold transform и тест к нему

--- LookGenerator/datasets/transforms.py
+++ LookGenerator/datasets/transforms.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 import torch
+import torch.nn as nn
 from torchvision import transforms
 
 
@@ -17,6 +20,26 @@ class MinMaxScale(object):
         self.max = image.max()
 
         min_tensor = torch.full(image.shape, self.min)
+        if self.max - self.min == 0:
+            raise ZeroDivisionError
 
         image = (image - min_tensor) / (self.max - self.min)
         return image
+
+
+class ThresholdTransform(object):
+    """Signum function transform"""
+    def __init__(self, threshold=0.5):
+        """
+        Args:
+            threshold: threshold to put 1 or 0. Value can be between 0 and 1
+        """
+        self.threshold = threshold
+
+    def __call__(self, image):
+        """
+        Args:
+            image: image to be transformed
+        """
+        return (image >= self.threshold).float()
+
--- LookGenerator/tests/transforms_test.py
+++ LookGenerator/tests/transforms_test.py
@@ -3,13 +3,21 @@ import numpy as np
 import torch
 from torchvision import transforms
 
-from LookGenerator.datasets.transforms import MinMaxScale
+from LookGenerator.datasets.transforms import MinMaxScale, ThresholdTransform
 
 
-def min_max_scale_test(image):
+def min_max_scale_test(image_):
     transform = MinMaxScale()
-    image = transform(image)
-    print(image)
+    image_ = transform(image_)
+    print(image_)
+    return image_
+
+
+def threshold_transform_test(image_):
+    transform = ThresholdTransform(threshold=0.8)
+    image_ = transform(image_)
+    print(image_)
+    return image_
 
 
 if __name__ == "__main__":
@@ -18,4 +26,5 @@ if __name__ == "__main__":
     image = image_1 + image_2
     print(image)
 
-    min_max_scale_test(image)
+    image = min_max_scale_test(image)
+    image = threshold_transform_test(image)
