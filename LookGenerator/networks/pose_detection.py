3994781379192692bf068e4e5a9051753331b99d
zen0no <dolgovyazovandrej@gmail.com>
2023-02-20 16:38:20 +0200

    Bugfix: исправлена ошибка с загрузкой posenet

--- LookGenerator/networks/pose_detection.py
+++ LookGenerator/networks/pose_detection.py
@@ -30,7 +30,7 @@ def load_posenet():
     """
         Loads posenet model
     """
-    path = os.path(PROJECT_ROOT, "weights", "posenet.pt")
+    path = os.path.join(PROJECT_ROOT, "weights", "posenet.pt")
     net = torch.load(path)
     return net
 
