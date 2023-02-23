a8430c2f996fe1e69f6e6358a201f110f2664406
zanoo <dmitryellison@gmail.com>
2023-02-23 17:08:23 +0300

    added gdown and created structure

--- LookGenerator/config/config.py
+++ LookGenerator/config/config.py
@@ -1,5 +1,4 @@
 import os
-
 import dotenv
 
 
@@ -10,3 +9,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 DATASET_DIR = os.path.join(PROJECT_ROOT, "datasets", "VITONHD")
 
 WEIGHTS_URL = os.getenv("WEIGHTS_URL")
+
+WEIGHTS_DIR = os.getenv("WEIGHTS_DIR")
+
+WEIGHTS_FILES_DICT = {os.path.join(WEIGHTS_DIR, "posenet.pt"):  os.getenv("POSENET_URL")}
--- LookGenerator/utils/enviroment.py
+++ LookGenerator/utils/enviroment.py
@@ -1,13 +1,19 @@
 import os
-from LookGenerator.config.config import WEIGHTS_URL, WEIGHTS_DIR
+import gdown
+from LookGenerator.config.config import WEIGHTS_URL, WEIGHTS_DIR, WEIGHTS_FILES_DICT
 
 
+def load_weights():
+    """ Check that weights exist
+        and download them in other way """
 
-def load_weights(): 
-    if os.path.exists(WEIGHTS_DIR):
-         return True
-    
-    else: 
+    if not os.path.exists(WEIGHTS_DIR):
+        assert "WEIGHTS_DIR is doesnt exist."
+        os.mkdir(WEIGHTS_DIR)
 
-        #Download files to WEITHGS_DIR from WEIGHTS_URL
-        pass
+    for file_dir, file_url in WEIGHTS_FILES_DICT:
+        if not os.path.exists(file_dir):
+            assert file_dir + " is doesnt exist. \n\tTrying to download:\n"
+            # TODO: download wiegths files
+        else:
+            print(file_dir + " downloaded.")
