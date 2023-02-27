1394776b030c5e900b06e2b532ac0973dfcddf15
Egor Prokopov <egorprokopov216@gmail.com>
2023-02-27 08:57:09 +0300

    Улучшил UNetTrainer

--- LookGenerator/networks/segmentation.py
+++ LookGenerator/networks/segmentation.py
@@ -1,3 +1,5 @@
+import datetime
+
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -5,7 +7,7 @@ from tqdm import tqdm
 
 from LookGenerator.networks.losses import FocalLoss
 from LookGenerator.networks.modules import Conv3x3, Conv5x5
-from LookGenerator.networks.utils import save_model
+from LookGenerator.networks.utils import save_model, _get_num_digits
 
 
 class UNet(nn.Module):
@@ -54,7 +56,7 @@ class UNet(nn.Module):
             Conv5x5(features[0], features[0], batch_norm=True, dropout=False, activation_func=nn.ReLU()),
             nn.Conv2d(features[0], out_channels, kernel_size=1)
         )
-        # self.sigmoid = nn.Sigmoid() - откомментить, если используется самописная функция активации
+        # self.sigmoid = nn.Sigmoid() #  - откомментить, если используется самописная функция активации
 
     def forward(self, x):
         """
@@ -95,13 +97,23 @@ class UNetTrainer:
     """
     Class for UNet training
     """
-    def __init__(self, model, optimizer, criterion, device='cpu', save_directory=r""):
-        self.model = model
+    def __init__(self, model_, optimizer, criterion, device='cpu', save_directory=r"", save_step=1, verbose=True):
+        """
+
+        Args:
+            model_: model to train
+            optimizer: model optimizer
+            criterion: loss function for this model
+            device: training device. Default: cpu
+            save_directory: Path for this training session directory. Default: ""
+            save_step: Step between epoch saves. Default: 1
+            verbose: If 'True', will print verbose output of the model
+        """
+        self.model = model_
         self.optimizer = optimizer
         self.criterion = criterion
         device = torch.device(device)
         self.device = device
-        self.save_directory = save_directory
         self.criterion.to(self.device)
 
         self.train_history_epochs = []
@@ -109,6 +121,10 @@ class UNetTrainer:
 
         self.train_history_batches = []
         self.val_history_batches = []
+
+        self.save_directory = save_directory
+        self.save_step = save_step
+        self.verbose = verbose
 
     def train(self, train_dataloader, val_dataloader, epoch_num=5):
         """
@@ -118,13 +134,47 @@ class UNetTrainer:
             val_dataloader: dataloader for validation
             epoch_num: number of epoch for training and validation
         """
-        for epoch in range(epoch_num):
-            self._train_epoch(train_dataloader, epoch, epoch_num)
-            self._val_epoch(val_dataloader, epoch, epoch_num)
-            save_model(model.to('cpu'),
-                       path=f"{self.save_directory}\\unet_epoch_{self._epoch_string(epoch, epoch_num)}.pt")
+        start = datetime.datetime.now()
+        print("start time", start.strftime("%d-%m-%Y %H:%M"))
 
-    def _train_epoch(self, train_dataloader, epoch: int, epoch_num: int):
+        for epoch in range(epoch_num):
+            # Train
+            train_loss = self._train_epoch(train_dataloader)
+            self.train_history_epochs.append(train_loss)
+            if self.verbose:
+                print(f'Epoch {epoch} of {epoch_num - 1}, train loss: {train_loss:.5f}')
+                now = datetime.datetime.now()
+                print("Epoch end time", now.strftime("%d-%m-%Y %H:%M"))
+            torch.cuda.empty_cache()
+
+            # Validation
+            val_loss = self._val_epoch(val_dataloader)
+            self.val_history_epochs.append(val_loss)
+            if self.verbose:
+                print(f'Epoch {epoch} of {epoch_num - 1}, val loss: {val_loss:.5f}')
+                now = datetime.datetime.now()
+                print("Epoch end time", now.strftime("%d-%m-%Y %H:%M"))
+            torch.cuda.empty_cache()
+
+            # Save
+            if self.save_step == 0 or self.save_directory == "":
+                continue
+            if (epoch + 1) % self.save_step == 0:
+                save_model(model.to('cpu'), path=f"{self.save_directory}\\unet_epoch_{self._epoch_string(epoch, epoch_num)}.pt")
+
+        now = datetime.datetime.now()
+        print("end time", now.strftime("%d-%m-%Y %H:%M"))
+        print("delta", now - start)
+
+    def _train_epoch(self, train_dataloader):
+        """
+        Method for epoch training
+        Args:
+            train_dataloader:  train dataloader
+
+        Returns: train loss
+
+        """
         self.model = self.model.to(self.device)
 
         train_running_loss = 0.0
@@ -144,28 +194,17 @@ class UNetTrainer:
             self.train_history_batches.append(loss_number)
 
         train_loss = train_running_loss / len(train_dataloader)
-        self.train_history_epochs.append(train_loss)
-        print(f'Epoch {epoch} of {epoch_num - 1}, train loss: {train_loss:.5f}')
-        torch.cuda.empty_cache()
+        return train_loss
 
-    def _epoch_string(self, epoch, epoch_num):
-        num_digits_epoch_num = self._get_num_digits(epoch_num)
-        num_digits_epoch = self._get_num_digits(epoch)
+    def _val_epoch(self, val_dataloader):
+        """
+        Method for epoch validation
+        Args:
+            val_dataloader:
 
-        epoch_string = "0"*(num_digits_epoch_num - num_digits_epoch) + str(epoch)
-        return epoch_string
+        Returns: validation loss
 
-    # TODO: перенести в utils
-    @staticmethod
-    def _get_num_digits(a):
-        num = 0
-        while a > 0:
-            num += 1
-            a = a // 10
-
-        return a
-
-    def _val_epoch(self, val_dataloader, epoch: int, epoch_num: int):
+        """
         val_running_loss = 0.0
         model.eval()
         for data, targets in tqdm(val_dataloader):
@@ -179,9 +218,26 @@ class UNetTrainer:
             self.val_history_batches.append(loss_number)
 
         val_loss = val_running_loss / len(val_dataloader)
-        self.val_history_epochs.append(val_loss)
-        print(f'Epoch {epoch} of {epoch_num - 1}, val loss: {val_loss:.5f}')
-        torch.cuda.empty_cache()
+        return val_loss
+
+    @staticmethod
+    def _epoch_string(epoch, epoch_num):
+        """
+        Method to create a string form of current epoch number, using the same number
+        of digits for every training session
+
+        Args:
+            epoch: number of current epoch
+            epoch_num: number of epochs for this training session
+
+        Returns:
+
+        """
+        num_digits_epoch_num = _get_num_digits(epoch_num)
+        num_digits_epoch = _get_num_digits(epoch)
+
+        epoch_string = "0"*(num_digits_epoch_num - num_digits_epoch) + str(epoch)
+        return epoch_string
 
     def draw_history_plots(self, ):
         """
