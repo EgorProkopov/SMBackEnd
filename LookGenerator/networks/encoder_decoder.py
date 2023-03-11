94220d47b4ba2209fcd83b0e3a03daa9d683e18b
EgorProkopov <egorprokopov216@gmail.com>
2023-03-11 21:26:52 +0300

    Вернул сквозные связи и немного изменил архитектуру сети: чтобы избавиться от силуэта одежды добавил больше слоев в начало и конец сети

--- LookGenerator/networks/encoder_decoder.py
+++ LookGenerator/networks/encoder_decoder.py
@@ -13,8 +13,12 @@ class EncoderDecoder(nn.Module):
 
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv_module1 = Conv5x5(in_channels=in_channels, out_channels=64, activation_func=nn.LeakyReLU())
-        self.conv_module2 = Conv5x5(in_channels=64, out_channels=128, batch_norm=True, activation_func=nn.LeakyReLU())
+        self.conv_module1 = nn.Sequential(
+            Conv5x5(in_channels=in_channels, out_channels=64, activation_func=nn.LeakyReLU()),
+            Conv5x5(in_channels=64, out_channels=64, activation_func=nn.LeakyReLU())
+        )
+
+        self.conv_module2 = Conv3x3(in_channels=64, out_channels=128, batch_norm=True, activation_func=nn.LeakyReLU())
         self.conv_module3 = Conv3x3(in_channels=128, out_channels=256, batch_norm=True, activation_func=nn.LeakyReLU())
         self.conv_module4 = Conv3x3(in_channels=256, out_channels=512, batch_norm=True, activation_func=nn.LeakyReLU())
         self.conv_module5 = Conv3x3(in_channels=512, out_channels=512, batch_norm=True, activation_func=nn.LeakyReLU())
@@ -22,28 +26,28 @@ class EncoderDecoder(nn.Module):
         self.bottle_neck = Conv3x3(in_channels=512, out_channels=512,
                                    batch_norm=True, activation_func=nn.ReLU())
 
-        deconv_input_scaler = 1
-
         self.deconv_module1 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv_conv_module1 = Conv5x5(in_channels=512*deconv_input_scaler, out_channels=512,
+        self.deconv_conv_module1 = Conv3x3(in_channels=512*2, out_channels=512,
                                            batch_norm=True, activation_func=nn.ReLU())
 
         self.deconv_module2 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv_conv_module2 = Conv5x5(in_channels=512*deconv_input_scaler, out_channels=256,
+        self.deconv_conv_module2 = Conv3x3(in_channels=512*2, out_channels=256,
                                            batch_norm=True, activation_func=nn.ReLU())
 
         self.deconv_module3 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv_conv_module3 = Conv5x5(in_channels=256*deconv_input_scaler, out_channels=128,
+        self.deconv_conv_module3 = Conv3x3(in_channels=256*2, out_channels=128,
                                            batch_norm=True, activation_func=nn.ReLU())
 
         self.deconv_module4 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv_conv_module4 = Conv5x5(in_channels=128*deconv_input_scaler, out_channels=64,
+        self.deconv_conv_module4 = Conv3x3(in_channels=128*2, out_channels=64,
                                            batch_norm=True, activation_func=nn.ReLU())
 
         self.deconv_module5 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.deconv_conv_module5 = Conv5x5(in_channels=64*deconv_input_scaler, out_channels=32,
-                                           batch_norm=True, activation_func=nn.ReLU())
+        self.deconv_conv_module5 = nn.Sequential(
+            Conv5x5(in_channels=64*2, out_channels=32, batch_norm=True, activation_func=nn.ReLU()),
+            Conv5x5(in_channels=32, out_channels=32, batch_norm=True, activation_func=nn.ReLU())
+        )
 
         # self.final_conv = Conv3x3(in_channels=32, out_channels=out_channels,
         #                           batch_norm=True, activation_func=nn.Tanh())
@@ -66,45 +70,45 @@ class EncoderDecoder(nn.Module):
         skip_connections = []
 
         out = self.conv_module1(x)
-        # skip_connections.append(out)
+        skip_connections.append(out)
         out = self.max_pool(out)
 
         out = self.conv_module2(out)
-        # skip_connections.append(out)
+        skip_connections.append(out)
         out = self.max_pool(out)
 
         out = self.conv_module3(out)
-        # skip_connections.append(out)
+        skip_connections.append(out)
         out = self.max_pool(out)
 
         out = self.conv_module4(out)
-        # skip_connections.append(out)
+        skip_connections.append(out)
         out = self.max_pool(out)
 
         out = self.conv_module5(out)
-        # skip_connections.append(out)
+        skip_connections.append(out)
         out = self.max_pool(out)
 
         out = self.bottle_neck(out)
 
         out = self.deconv_module1(out)
-        # out = torch.cat((out, skip_connections[3]), axis=1)
+        out = torch.cat((out, skip_connections[4]), axis=1)
         out = self.deconv_conv_module1(out)
 
         out = self.deconv_module2(out)
-        # out = torch.cat((out, skip_connections[2]), axis=1)
+        out = torch.cat((out, skip_connections[3]), axis=1)
         out = self.deconv_conv_module2(out)
 
         out = self.deconv_module3(out)
-        # out = torch.cat((out, skip_connections[1]), axis=1)
+        out = torch.cat((out, skip_connections[2]), axis=1)
         out = self.deconv_conv_module3(out)
 
         out = self.deconv_module4(out)
-        # out = torch.cat((out, skip_connections[0]), axis=1)
+        out = torch.cat((out, skip_connections[1]), axis=1)
         out = self.deconv_conv_module4(out)
 
         out = self.deconv_module5(out)
-        # out = torch.cat((out, skip_connections[0]), axis=1)
+        out = torch.cat((out, skip_connections[0]), axis=1)
         out = self.deconv_conv_module5(out)
 
         out = self.final_conv(out)
