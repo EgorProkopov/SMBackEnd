4b3a2fec2a9d2a927491859bdd35975a2cc5fe83
EgorProkopov <egorprokopov216@gmail.com>
2023-03-14 19:27:09 +0300

    добавил сигмоиду в конец энкодера-декодера

--- LookGenerator/networks/encoder_decoder.py
+++ LookGenerator/networks/encoder_decoder.py
@@ -53,7 +53,8 @@ class EncoderDecoder(nn.Module):
         #                           batch_norm=True, activation_func=nn.Tanh())
 
         self.final_conv = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=out_channels, kernel_size=1)
+            nn.Conv2d(in_channels=32, out_channels=out_channels, kernel_size=1),
+            nn.Sigmoid()
         )
 
     def forward(self, x):
