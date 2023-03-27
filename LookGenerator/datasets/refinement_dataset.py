1a3a00def1af2868b136f527477dd41ee279e620
Egor Prokopov <egorprokopov216@gmail.com>
2023-03-27 13:13:57 +0300

    Написал класс датасета для предобучения дискриминатора RefienementGAN'а

--- LookGenerator/datasets/refinement_dataset.py
+++ LookGenerator/datasets/refinement_dataset.py
@@ -59,3 +59,51 @@ class RefinementGANDataset(Dataset):
 
         return input_image, target_image
 
+
+class RefinementDiscriminatorDataset(Dataset):
+    """
+    Dataset for Refinement discriminator pretraining
+    """
+    def __init__(
+            self, real_images_dir: str,
+            fake_images_dir: str,
+            transform=None
+    ):
+        super(RefinementDiscriminatorDataset, self).__init__()
+
+        self.real_images_dir = real_images_dir
+        self.fake_images_dir = fake_images_dir
+
+        self.list_of_real = os.listdir(self.real_images_dir)
+        self.list_of_fake = os.listdir(self.fake_images_dir)
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.list_of_real) + len(self.list_of_fake)
+
+    def __getitem__(self, idx):
+        transform_to_tensor = transforms.ToTensor()
+        if idx < len(self.list_of_real):
+            input_image = transform_to_tensor(load_image(
+                self.real_images_dir, "", self.list_of_real[idx], ""
+            ))
+
+            if self.transform:
+                input_image = self.transform(input_image)
+
+            label = torch.Tensor([1])
+
+            return input_image, label
+
+        else:
+            input_image = transform_to_tensor(load_image(
+                self.fake_images_dir, "", self.list_of_fake[idx], ""
+            ))
+
+            if self.transform:
+                input_image = self.transform(input_image)
+
+            label = torch.Tensor([-1])
+
+            return input_image, label
