a1ffcffca6741d115e4347bd3347a64781055796
Egor Prokopov <egorprokopov216@gmail.com>
2023-04-08 11:03:48 +0300

    Написал модель генератора и дискриминатора для ГАНа
    Генератор сделал на основе бывшего энкодера-декодера, старый энкодер-декодер оставил на месте

---
+++ LookGenerator/networks/FineGAN.py
@@ -0,0 +1,182 @@
+import torch
+import torch.nn as nn
+
+from LookGenerator.networks.modules import Conv3x3, Conv5x5
+
+
+class EncoderDecoderGenerator(nn.Module):
+    """Generator part of the GAN """
+
+    def __init__(self, in_channels=22, out_channels=3):
+        super(EncoderDecoderGenerator, self).__init__()
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv_module1 = nn.Sequential(
+            Conv5x5(in_channels=in_channels, out_channels=64, activation_func=nn.LeakyReLU(), res_conn=True)
+        )
+
+        self.conv_module2 = Conv3x3(in_channels=64, out_channels=128, batch_norm=True, activation_func=nn.LeakyReLU())
+        self.conv_module3 = Conv3x3(in_channels=128, out_channels=256, batch_norm=True, activation_func=nn.LeakyReLU())
+        self.conv_module4 = Conv3x3(in_channels=256, out_channels=512, batch_norm=True, activation_func=nn.LeakyReLU())
+        self.conv_module5 = Conv3x3(in_channels=512, out_channels=512, batch_norm=True, activation_func=nn.LeakyReLU())
+
+        self.bottle_neck = Conv3x3(in_channels=512, out_channels=512,
+                                   batch_norm=True, activation_func=nn.ReLU())
+
+        self.deconv_module1 = nn.UpsamplingNearest2d(scale_factor=2)
+        self.deconv_conv_module1 = Conv3x3(in_channels=512 * 2, out_channels=512,
+                                           batch_norm=True, activation_func=nn.ReLU())
+
+        self.deconv_module2 = nn.UpsamplingNearest2d(scale_factor=2)
+        self.deconv_conv_module2 = Conv3x3(in_channels=512 * 2, out_channels=256,
+                                           batch_norm=True, activation_func=nn.ReLU())
+
+        self.deconv_module3 = nn.UpsamplingNearest2d(scale_factor=2)
+        self.deconv_conv_module3 = Conv3x3(in_channels=256 * 2, out_channels=128,
+                                           batch_norm=True, activation_func=nn.ReLU())
+
+        self.deconv_module4 = nn.UpsamplingNearest2d(scale_factor=2)
+        self.deconv_conv_module4 = Conv3x3(in_channels=128 * 2, out_channels=64,
+                                           batch_norm=True, activation_func=nn.ReLU())
+
+        self.deconv_module5 = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.deconv_conv_module5 = nn.Sequential(
+            Conv5x5(in_channels=64 * 2, out_channels=32, batch_norm=True, activation_func=nn.ReLU(), res_conn=True)
+        )
+
+        self.final_conv = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=out_channels, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        """
+        Forward propagation method of neural network.
+        Args:
+            x: mini-batch of data
+
+        Returns:
+            Tensor of packed decoded human and clothes mask.
+            First 3 channels for decoded human
+        """
+        skip_connections = []
+
+        out = self.conv_module1(x)
+        skip_connections.append(out)
+        out = self.max_pool(out)
+
+        out = self.conv_module2(out)
+        skip_connections.append(out)
+        out = self.max_pool(out)
+
+        out = self.conv_module3(out)
+        skip_connections.append(out)
+        out = self.max_pool(out)
+
+        out = self.conv_module4(out)
+        skip_connections.append(out)
+        out = self.max_pool(out)
+
+        out = self.conv_module5(out)
+        skip_connections.append(out)
+        out = self.max_pool(out)
+
+        out = self.bottle_neck(out)
+
+        out = self.deconv_module1(out)
+        out = torch.cat((out, skip_connections[4]), axis=1)
+        out = self.deconv_conv_module1(out)
+
+        out = self.deconv_module2(out)
+        out = torch.cat((out, skip_connections[3]), axis=1)
+        out = self.deconv_conv_module2(out)
+
+        out = self.deconv_module3(out)
+        out = torch.cat((out, skip_connections[2]), axis=1)
+        out = self.deconv_conv_module3(out)
+
+        out = self.deconv_module4(out)
+        out = torch.cat((out, skip_connections[1]), axis=1)
+        out = self.deconv_conv_module4(out)
+
+        out = self.deconv_module5(out)
+        out = torch.cat((out, skip_connections[0]), axis=1)
+        out = self.deconv_conv_module5(out)
+
+        out = self.final_conv(out)
+
+        return out
+
+
+class Discriminator(nn.Module):
+    """
+    Discriminator of the GAN network
+    """
+    def __init__(self, in_channels=3, batch_norm=True, sigmoid=True):
+        """
+
+        Args:
+            in_channels: number of input channels
+            sigmoid: if 'True', puts the sigmoid activation function in the end
+        """
+        super(Discriminator, self).__init__()
+
+        self.features = nn.Sequential(
+            # input size: in_channels x 256x192
+            Conv5x5(in_channels=in_channels, out_channels=16,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            Conv5x5(in_channels=16, out_channels=16,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # size: 16x128x96
+
+            Conv5x5(in_channels=16, out_channels=32,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            Conv5x5(in_channels=32, out_channels=32,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # size: 32x64x48
+
+            Conv5x5(in_channels=32, out_channels=64,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            Conv5x5(in_channels=64, out_channels=64,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # size: 64x32x24
+
+            Conv5x5(in_channels=64, out_channels=128,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            Conv5x5(in_channels=128, out_channels=128,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # size: 128x16x12
+
+            Conv5x5(in_channels=128, out_channels=256,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            Conv5x5(in_channels=256, out_channels=256,
+                    batch_norm=batch_norm, activation_func=nn.LeakyReLU(), res_conn=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # output size: 256x8x6
+
+        )
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=256*8*6, out_features=1024),
+            nn.Dropout(p=0.5),
+            nn.LeakyReLU(),
+
+            nn.Linear(in_features=1024, out_features=1)
+        )
+
+        self.sigmoid = sigmoid
+
+    def forward(self, x):
+        out = self.features(x)
+        out = self.flatten(out)
+        out = self.classifier(out)
+        if self.sigmoid:
+            out = nn.functional.sigmoid(out)
+        return out
+
