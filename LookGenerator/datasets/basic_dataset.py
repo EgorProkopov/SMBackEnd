07f3b04c15d1911b9976c34f5ebe943d1d3ba80c
sidsnow <obobojk@gmail.com>
2023-04-20 15:58:23 +0300

    базовый датасет стал более базовым

--- LookGenerator/datasets/basic_dataset.py
+++ LookGenerator/datasets/basic_dataset.py
@@ -5,42 +5,64 @@ from LookGenerator.datasets.utils import load_image
 
 
 class BasicDataset(Dataset):
-    """ Dataset for opening images and returning it, without targets"""
+    """ Basic dataset with transforms for images and targets"""
 
-    def __init__(self, root_dir: str, dir_name: str, transform_input=None):
+    def __init__(self,
+                 root_dir: str,
+                 input_dir_name: str,
+                 target_dir_name: str,
+                 transform_input=None,
+                 transform_target=None):
         """
 
         Args:
             root_dir: dir where a folder is
-            dir_name: folder with images
+            input_dir_name: folder with inputs
+            target_dir_name: folder with targets
             transform_input: transform for input
+            transform_target: transform for target
         """
         super().__init__()
 
         self.root_dir = root_dir
-        self.dir_name = dir_name
+        self.input_dir_name = input_dir_name
+        self.target_dir_name = target_dir_name
         self.transform_input = transform_input
+        self.transform_target = transform_target
 
-        root = os.path.join(root_dir, dir_name)
+        input_root = os.path.join(root_dir, input_dir_name)
+        target_root = os.path.join(root_dir, target_dir_name)
 
-        names_of_files = os.listdir(root)
+        input_files = os.listdir(input_root)
+        target_files = os.listdir(target_root)
 
-        self._files_list = [name.split('.')[0] for name in names_of_files]
-        self._extensions_list = [name.split('.')[1] for name in names_of_files]
+        self._input_files_list = [name.split('.')[0] for name in input_files]
+        self._input_extensions_list = [name.split('.')[1] for name in input_files]
+
+        self._target_files_list = [name.split('.')[0] for name in target_files]
+        self._target_extensions_list = [name.split('.')[1] for name in target_files]
 
     def __getitem__(self, idx):
         to_tensor = ToTensor()
 
         input_ = load_image(self.root_dir,
-                            self.dir_name,
-                            self._files_list[idx],
-                            '.' + self._extensions_list[idx])
+                            self.input_dir_name,
+                            self._input_files_list[idx],
+                            '.' + self._input_extensions_list[idx])
+        target = load_image(self.root_dir,
+                            self.target_dir_name,
+                            self._target_files_list[idx],
+                            '.' + self._target_extensions_list[idx])
         input_ = to_tensor(input_)
+        target = to_tensor(target)
 
         if self.transform_input:
             input_ = self.transform_input(input_)
 
-        return input_
+        if self.transform_target:
+            target = self.transform_target(target)
+
+        return input_, target
 
     def __len__(self):
-        return len(self._files_list)
+        return len(self._input_files_list)
