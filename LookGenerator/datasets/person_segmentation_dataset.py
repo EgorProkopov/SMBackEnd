e4203ddf98e2806aaf7a26c8cd3cb1ef02ab2dc5
sidsnow <obobojk@gmail.com>
2023-05-26 19:59:06 +0300

    возвращение мультиканала

--- LookGenerator/datasets/person_segmentation_dataset.py
+++ LookGenerator/datasets/person_segmentation_dataset.py
@@ -16,19 +16,19 @@ class PersonSegmentationDatasetMultichannel(Dataset):
     Might be deprecated soon
     """
 
-    def __init__(self, image_dir: str, transform_input=None, transform_mask=None, augment=None):
+    def __init__(self, image_dir: str, transform_input=None, transform_output=None, augment=None):
         """
         Args:
             image_dir: Directory with all images
             transform_input: A transform to be applied on input images. Default: None
-            transform_mask: A transform to be applied on mask of image. Default: None
+            transform_output: A transform to be applied on mask of image. Default: None
         """
 
         super().__init__()
 
         self.root = image_dir
         self.transform_input = transform_input
-        self.transform_mask = transform_mask
+        self.transform_output = transform_output
         self.augment = augment
 
         list_of_files = os.listdir(image_dir + r"\image")
@@ -42,12 +42,14 @@ class PersonSegmentationDatasetMultichannel(Dataset):
         Returns: A Pair of X and y objects for segmentation
         """
 
+        seed = torch.random.seed()
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         to_tensor = ToTensor()
 
-        input_ = np.array(load_image(self.root, "image", self._files_list[idx], ".jpg"))
+        input_ = load_image(self.root, "image", self._files_list[idx], ".jpg")
         target = []
 
         channel_list = os.listdir(os.path.join(
@@ -58,28 +60,24 @@ class PersonSegmentationDatasetMultichannel(Dataset):
 
         channel_files_list = [file.split('.')[0] for file in channel_list]
 
-        # for channel in channel_files_list:
-        #     target.append(convert_channel(load_image(self.root,
-        #                                              os.path.join("image-parse-v3-multichannel", self._files_list[idx]),
-        #                                              channel,
-        #                                              ".png")))
-
-        if self.augment:
-            transformed = self.augment(image=input_, masks=target)
-            input_ = transformed['image']
-            target = transformed['masks']
+        for channel in channel_files_list:
+            target.append(to_tensor((load_image(self.root,
+                                                os.path.join("image-parse-v3-multichannel",
+                                                             self._files_list[idx]),
+                                                channel,
+                                                ".png"))))
 
         input_ = to_tensor(input_)
-        target = to_tensor(np.array(target))
-
-        target = torch.transpose(target, 0, 1)
-        target = torch.transpose(target, 1, 2)
+        target = to_tensor(target)
 
         if self.transform_input:
+            torch.manual_seed(seed)
             input_ = self.transform_input(input_)
 
         if self.transform_mask:
+            torch.manual_seed(seed)
             target = self.transform_mask(target)
+
         return input_.float(), target.float()
 
     def __len__(self):
