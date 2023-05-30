6be593e463fa6805a57ae43440a034ac62592a84
EgorProkopov <egorprokopov216@gmail.com>
2023-05-30 10:36:31 +0300

    написано строковое представление класса автокодировщика и тест к нему

--- LookGenerator/tests/autoencoder_tests.py
+++ LookGenerator/tests/autoencoder_tests.py
@@ -23,6 +23,21 @@ def shape_test(input_tensor):
     print("Test 1 Complete")
 
 
+def str_test():
+    autoencoder = ClothAutoencoder(
+        in_channels=3,
+        out_channels=3,
+        features=(8, 16, 32, 64),
+        latent_dim_size=128,
+        encoder_activation_func=nn.LeakyReLU(),
+        decoder_activation_func=nn.ReLU()
+    )
+
+    print("Test 2 Result: ")
+    print(str(autoencoder))
+
+
 if __name__ == "__main__":
     input_tensor = torch.zeros((1, 3, 256, 192))
     shape_test(input_tensor)
+    str_test()
