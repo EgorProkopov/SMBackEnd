8cac445fbef29460f7b367632d8869d24602e699
Egor Prokopov <egorprokopov216@gmail.com>
2023-06-01 13:38:13 +0300

    Вынес тренировщик с маской в класс наследник простого тренировщика

--- LookGenerator/networks/trainer.py
+++ LookGenerator/networks/trainer.py
@@ -160,7 +160,7 @@ class Trainer:
         Draws plots of train and validation
 
         Args:
-            epochs: TODO: if 'True', draws history plots by epochs, else by batches
+            epochs: if 'True', draws history plots by epochs, else by batches
         """
         if epochs:
             plt.plot(self.train_history_epochs, label="train")
@@ -176,6 +176,84 @@ class Trainer:
                       f"Criterion: \n\t{str(type(self.criterion))}\n" \
                       f"Optimizer: \n\t{str(type(self.optimizer))}"
         return description
+
+
+class TrainerWithMask(Trainer):
+    def __init__(
+            self, model_,
+            optimizer, criterion,
+            device='cpu', save_directory=r"",
+            save_step=1, verbose=True
+    ):
+        """
+        Args:
+            model_: model to train
+            optimizer: model optimizer
+            criterion: loss function for this model
+            device: training device. Default: cpu
+            save_directory: Path for this training session directory. Default: ""
+            save_step: Step between epoch saves. Default: 1
+            verbose: If 'True', will print verbose output of the model
+        """
+
+        super().__init__(model_, optimizer, criterion, device, save_directory, save_step, verbose)
+
+    def _train_epoch(self, train_dataloader):
+        """
+        Method for epoch training
+        Args:
+            train_dataloader:  train dataloader
+
+        Returns: train loss
+
+        """
+        self.model = self.model.to(self.device)
+
+        train_running_loss = 0.0
+        self.model.train()
+        for data, mask, targets in tqdm(train_dataloader):
+            data = data.to(self.device)
+            mask = mask.to(self.device)
+            targets = targets.to(self.device)
+            outputs = self.model(data)
+
+            self.optimizer.zero_grad()
+            loss = self.criterion(outputs, mask, targets)
+            loss.backward()
+            self.optimizer.step()
+
+            loss_number = loss.item()
+            train_running_loss += loss_number
+            self.train_history_batches.append(loss_number)
+
+        train_loss = train_running_loss / len(train_dataloader)
+        return train_loss
+
+    def _val_epoch(self, val_dataloader):
+        """
+        Method for epoch validation
+        Args:
+            val_dataloader:
+
+        Returns: validation loss
+
+        """
+        val_running_loss = 0.0
+        self.model.eval()
+        for data, mask, targets in tqdm(val_dataloader):
+            data = data.to(self.device)
+            mask = mask.to(self.device)
+            targets = targets.to(self.device)
+            outputs = self.model(data)
+
+            loss = self.criterion(outputs, mask, targets)
+            loss_number = loss.item()
+            val_running_loss += loss_number
+            self.val_history_batches.append(loss_number)
+
+        val_loss = val_running_loss / len(val_dataloader)
+        return val_loss
+
 
 class GANTrainer:
     def __init__(
