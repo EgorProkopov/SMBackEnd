ab78060c78661fa059677d2010de6ba7a9a3861b
EgorProkopov <egorprokopov216@gmail.com>
2023-06-02 14:29:39 +0300

    захардкодил перенос сеток на девайс

--- LookGenerator/networks/trainer.py
+++ LookGenerator/networks/trainer.py
@@ -555,6 +555,8 @@ class WGANGPTrainer:
         return loss_real, loss_fake, loss_d, loss_g
 
     def _train_discriminator(self, input_images, real_images):
+        self.discriminator = self.discriminator.to(self.device)
+        self.generator = self.generator.to(self.device)
         self.discriminator.train()
         self.generator.eval()
 
@@ -592,6 +594,8 @@ class WGANGPTrainer:
         self.optimizer_discriminator.step()
 
     def _train_generator(self, input_images, real_images):
+        self.discriminator = self.discriminator.to(self.device)
+        self.generator = self.generator(self.device)
         self.discriminator.eval()
         self.generator.train()
 
